e7c76d0bb3e56bbedfa533931a5fa47e54f8f7d9
Abhisek <abhisekbunty94@gmail.com>
2018-06-15 06:09:52 +0530

    Folder structurisation complete

---
+++ Starter/DemoTests/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoTests
+//
+//  Created by Kuliza-241 on 6/14/18.
+//  Copyright © 2018 Abhisek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
