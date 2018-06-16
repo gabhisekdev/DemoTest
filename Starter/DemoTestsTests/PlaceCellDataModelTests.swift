0d669c03f120d00d202689652deaf9d73e532c9a
Abhisek <abhisekbunty94@gmail.com>
2018-06-16 07:49:05 +0530

    Missing files added

--- Starter/DemoTestsTests/PlaceCellDataModelTests.swift
+++ Starter/DemoTestsTests/PlaceCellDataModelTests.swift
@@ -1,22 +1,24 @@
 //
 //  PlaceCellDataModelTests.swift
-//  DemoTestTests
+//  DemoTestsTests
 //
-//  Created by Abhisek on 6/10/18.
+//  Created by Kuliza-241 on 6/16/18.
 //  Copyright © 2018 Abhisek. All rights reserved.
 //
 
 import XCTest
-@testable import DemoTests
 
 class PlaceCellDataModelTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
+   
 }
